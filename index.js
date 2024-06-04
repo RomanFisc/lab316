58db5f5d52a839a386d21d1fbffcc93e2de99b97
RomanFisc <romanfisc@gmail.com>
2024-06-04 09:29:41 -0400

    h1 and main background set

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import "styles.css";
+
+
+let mainEL = document.querySelector('main');
+mainEL.style.backgroundColor = 'var(--main-bg)';
+
+let header1 = document.createElement('h1');
+header1.innerText = "Dom Manipulation";
+
+mainEL.appendChild(header1);
